d36dd17881652c48c5cd605544fc3919eb051456
Name <tumblegamer@gmail.com>
2020-10-11 16:05:09 +0100

    update

--- EchoLocation.user.js
+++ EchoLocation.user.js
@@ -2,7 +2,7 @@
 // @name         EchoLocation
 // @description  VCs per Room
 // @author       Tumble
-// @version      0.0.1.2
+// @version      0.0.2.2
 // @match        https://boxcritters.com/play/
 // @match        https://boxcritters.com/play/?*
 // @match        https://boxcritters.com/play/#*
@@ -13,41 +13,99 @@
 // @require      https://github.com/SArpnt/joinFunction/raw/master/script.js
 // @require      https://github.com/SArpnt/EventHandler/raw/master/script.js
 // @require      https://github.com/SArpnt/cardboard/raw/master/script.user.js
-// grant none
 // ==/UserScript==
 
 cardboard.register("ECHO_LOCATION");
+console.log("[Echo Location] By TumbleGamer")
+console.log = (...p) => {
+	p.unshift("[EL]");
+	console.debug(...p)
+};
 
-var EchoLocation = {};
+var EchoLocation = {
+	delay: 100,
+	emit: function (...p) {
+		this.socket.emit(...p)
+	},
+	on: function (...p) {
+		this.socket.on(...p)
+	},
+	login: function () {
+		if (!this.world) return;
+		let id = this.world.player.playerId;
+		console.log("Logging in as", id)
+		this.emit("login", id)
+	}
+};
 
-cardboard.on("runScripts",function() {
-	console.log(io);
-	var socket = io("tumble-room-vc.herokuapp.com")
+async function playBuffer(arrayBuffer) {
+	var audioContext = new AudioContext();
+	//var arrayBuffer = await blob.arrayBuffer();
+	var audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+	var bufferSource = audioContext.createBufferSource();
+	bufferSource.buffer = audioBuffer;
+	bufferSource.connect(audioContext.destination);
+	bufferSource.start();
+}
 
-	socket.on("connect",function() {
-		console.log("server connnected")
+function setupStream(mediaStream) {
+	var mediaRecorder = new MediaRecorder(mediaStream);
+	mediaRecorder.onstart = function (e) {
+		this.chunks = [];
+	};
+	mediaRecorder.ondataavailable = function (e) {
+		this.chunks.push(e.data);
+	};
+	mediaRecorder.onstop = function (e) {
+		var blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' });
+		console.log(blob);
+		EchoLocation.emit('voice', blob);
 
-		alert("Please join the voice channel in BCMC called start")
+		
+		EchoLocation.mediaRecorder.start();
+		setTimeout(()=>{
+			EchoLocation.mediaRecorder.stop()
+		},500)
+	};
+	mediaRecorder.start();
+	setTimeout(()=>{
+		EchoLocation.mediaRecorder.stop()
+	},EchoLocation.delay)
+	EchoLocation.mediaRecorder = mediaRecorder;
+}
 
-		while(!discord) {
-		var discord = prompt("Discord Userame#1234")
-		}
-		var code = Math.floor(1000 + Math.random() * 9000).toString();
+function setupMicrophone() {
+	console.log("setting up mic");
+	navigator.getUserMedia({ audio: true }, setupStream, console.log);
+}
+
+setupMicrophone();
 
 
-		socket.emit("login",{discord,code})
-		alert("Your code is" + code);
 
+cardboard.on("runScripts", function () {
+	if (io) console.log("Socket.io's 'io' variable has been found.");
+	var url = "tumble-room-vc.herokuapp.com"
+	//var url = "ws://localhost:3000"
+	EchoLocation.socket = io(url)
+
+	EchoLocation.on("connect", function () {
+		console.log("Connected to", url)
 	})
-
-	EchoLocation.socket = socket;
+	EchoLocation.on("voice",function(arrayBuffer)  {
+		playBuffer(arrayBuffer);
+	})
 })
-
-
-cardboard.on("worldSocketCreated",(world,socket)=>{
-	
-cardboard.on("joinRoom",(r)=>{
-	EchoLocation.socket.emit("joinRoom",r.room.roomId)
+cardboard.on("worldCreated", (world) => {
+	console.log("World Created")
+	EchoLocation.world = world;
 })
-
+cardboard.on("worldSocketCreated", (world, socket) => {
+	socket.on("joinRoom", (r) => {
+		console.log("Joined Room: " + r.roomId)
+		EchoLocation.emit("joinRoom", r.roomId)
+	})
 })
+cardboard.on("login", EchoLocation.login)
+
+unsafeWindow.EchoLocation = EchoLocation;
--- index.js
+++ index.js
@@ -1,91 +1,12 @@
-const Discord = require("discord.js");
 const webServer = require("tn-webserver");
 const socketIo = require("socket.io");
-const { Dir } = require("fs");
 
-const token = process.env.DISCORD_TOKEN || require('./config/token.js').token;
-const client = new Discord.Client(token);
-var server = webServer(function(){},{},19132);
-
-var guildId = "567030108003631104";
-var categoryId = "758816018436456469";
-var memberId="567032362810998824";
-
-var rooms = {};
-
-var onDM = function(message){}
 
 function createBinder(instance) {
 	return (function(a,...p){
 		return this[a].bind(this,...p);
 	}).bind(instance);
 }
-
-/**
- * @returns {Discord.Guild}
- */
-function getServer () {
-	return client.guilds.cache.get(guildId);
-}
-
-function getCategory() {
-	var bcmc = getServer();
-	return  bcmc.channels.cache.get(categoryId)
-}
-
-async function createChannel(name,perms= {allow: ["SPEAK","VIEW_CHANNEL"],deny:["CONNECT"]}) {
-	var server = getServer();
-	var category = getCategory();
-
-	var channel = await server.channels.create(name,{parent:category,type:"voice",permissionOverwrites: [
-		Object.assign({
-		  id: memberId
-		},perms),{
-		   id:client.user.id,
-		   allow:["MANAGE_CHANNELS","MOVE_MEMBERS"]
-	   }
-	 ]});
-	return channel;
-}
-
-async function setupCategory() {
-	var category = getCategory()
-	category.children.forEach(async c =>await c.delete() );
-	await createChannel("Start",{
-		allow:["CONNECT",'VIEW_CHANNEL'],
-		deny:["SPEAK"]
-	})
-}
-
-async function moveRoom(userId,roomName) {
-	console.log(userId,roomName)
-	var bcmc = getServer();
-	var member = bcmc.members.cache.get(userId);
-	var voiceChannel;
-	if(rooms[roomName]) {
-		voiceChannel = bcmc.channels.cache.get(rooms[roomName]);
-	} else {
-		voiceChannel = await createChannel(roomName);
-		rooms[roomName] = voiceChannel.id;
-	}
-	member.voice.setChannel(voiceChannel, "room change")
-}
-
-client.on('ready', async () => {
-	client.user.setPresence({ game: { name: 'Room Changes', type: "Watching", }, status: 'online' });
-	console.log(`Logged in as ${client.user.tag}!`);
-	await setupCategory();
-
-});
-
-client.on("message",function(message) {
-	if(message.author.id != client.user.id) {
-		return;
-	}
-	if(message.channel.type=="dm"){
-		onDM(message)
-	};
-})
 
 
 class Client {
@@ -99,7 +20,8 @@ class Client {
 		this.bindSocket("disconnect")
 		this.bindSocket("login")
 		this.bindSocket("joinRoom")
-		console.log("Client Connected:", this.name);
+		this.bindSocket("voice")
+		console.debug("Client Connected:", this.name);
 	}
 	getID() {
 		return this.socket.id;
@@ -108,8 +30,12 @@ class Client {
 		this.socket.emit(...a);
 	}
 
+	roomEmit(...a) {
+		this.socket.to(this.room).emit(...a);
+	}
+
 	disconnect() {
-		console.log("Client Disconnected:",this.name);(this)
+		console.debug("Client Disconnected:",this.name);(this)
 	}
 	bindSocket(name) {
 		var t = this;
@@ -119,22 +45,25 @@ class Client {
 		});
 	}
 
-	login({discord,code}) {
-		if(!discord)return;
-		var user = client.users.cache.find(u=>u.tag==discord);
-		user.send("What is your code?")
-		var c = this;
-		onDM = function(message) {
-			if(message.content == code) {
-				user.send("Thank you!");
-				c.discord = user.id;
-
-			}
+	login(bcid) {
+		console.debug("Client " + this.getID() + " has loogged in with Box Critters Id of " + bcid)
+		this.bcid = bcid;
 	}
 
 	joinRoom(room) {
-		console.log("Join room " + room);
-		moveRoom(this.discord,room);
+		console.debug()
+		this.socket.join(room);
+		this.room = room;
+	}
+
+	move({x,y}) {
+		this.x = x;
+		this.y = y
+	}
+
+	voice(data) {
+		var bcid = this.bcid;
+		this.roomEmit("voice",{bcid,data});
 	}
 }
 
@@ -153,5 +82,6 @@ class Server {
 	}
 }
 
+
+var server = webServer(function(){},{});
 new Server(server)
-client.login(token);
