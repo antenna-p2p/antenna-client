3c86166de6524556ad530b4860f103dead89b533
Tumble <tumblegamer@gmail.com>
2020-10-22 14:38:26 +0100

    microphone

--- client.js
+++ client.js
@@ -1,19 +1,19 @@
-var client = new AntennaClient({})
+var client = new AntennaClient({});
 
 client.setupSockets();
-var startBtn =  document.querySelector("button")
+var startBtn = document.querySelector("button");
 
 startBtn.addEventListener("click", () => {
 	startBtn.remove();
-	client.setupMic()
-})
+	client.setupMicrophone();
+});
 var roomForm = document.getElementById("room-form");
-roomForm.querySelector("button").addEventListener("click",() =>{
-	if(client.audioContext.state!=="running") return;
+roomForm.querySelector("button").addEventListener("click", () => {
+	if (client.audioContext.state !== "running") return;
 	var roomId = roomForm.querySelector("input#roomId").value;
-	console.log("Joining room",roomId)
-	client.joinRoom(roomId)
-})
+	console.log("Joining room", roomId);
+	client.joinRoom(roomId);
+});
 window.onunload = window.onbeforeunload = () => {
 	client.close();
 };
